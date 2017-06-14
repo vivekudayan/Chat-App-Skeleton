36ae2f2f2f3c247adbf0be64f2a7787a3afb75c0
Vivek <vivekudayanvarkala@gmail.com>
2017-06-14 14:09:01 +0530

    * Added Whiteboard utility with socket

--- app.js
+++ app.js
@@ -11,12 +11,19 @@ http.listen(3000, function(){
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');  
+	
+  console.log('user connected'+socket.id);  
+  
   socket.emit('init', backData);
+  
   socket.on('message', function(msg){
 	backData.push(msg);
     console.log('message: ' + msg);
 	io.emit('message', msg);
+  });
+  
+  socket.on('draw',function(msg){
+	  socket.broadcast.emit('draw', msg); 
   });
   
 });
