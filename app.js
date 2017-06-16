579e75884afba0659bfa2c9b99384443e2978d68
Vivek <vivekudayanvarkala@gmail.com>
2017-06-16 10:21:49 +0530

    Added Css framework skeleton.css

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+app.use(express.static('public'))
 var backData= [];
 app.get('/', function(req, res){
   res.sendFile(__dirname+'/views/index.html');
